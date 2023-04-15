5be6204b3687e9538d2678cce6257bb2a7f8a268
TomVerkon <tverkon@gmail.com>
2023-04-15 12:37:26 -0400

    Initial auth service setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
 
-app.listen(300, () => {
-  console.log('Listening on port 3000');
+app.get('/api/users/currentuser', (req: Request, res: Response) => {
+  res.send('Hi There!!!!!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!');
 });
