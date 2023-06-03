6da0772ef6075abd387a0c13ded2c6e646641044
TomVerkon <tverkon@gmail.com>
2023-06-03 16:28:53 -0400

    updated common module, added prop to Tickets.TicketDoc

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,35 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, OrderCreatedEvent, NotFoundError } from "@tverkon-ticketing/common";
+import { Ticket } from "../../model/ticket";
+import { queueGroupName } from "./queue-group-name";
+import mongoose from "mongoose";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { natsWrapper } from "../../nats-wrapper";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // Find the ticket the order is using
+    const ticket = await Ticket.findById(data.ticket.id);
+    // if no ticket, throw error
+    if (!ticket) throw new NotFoundError();
+    // mark the ticket as reserved by populating the orderId
+    ticket.set({ orderId: data.id });
+    // save the ticket
+    await ticket.save();
+
+    // let order service know that we updated the ticket
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      version: ticket.version,
+    });
+
+    // ack the message
+    msg.ack();
+  }
+}
