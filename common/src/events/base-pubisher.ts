b585a95739e45015c415cdc27c0f8208b3a44bf5
TomVerkon <tverkon@gmail.com>
2023-06-06 13:00:19 -0400

    Updates

--- common/src/events/base-pubisher.ts
+++ common/src/events/base-pubisher.ts
@@ -11,7 +11,7 @@ interface Event {
 export abstract class Publisher<T extends Event> {
   abstract subject: T['subject'];
   protected client: Stan;
-  private logMsgs = process.env.LOG_MSGS;
+  private logMsgs = process.env.LOG_MSGS === 'true' ? true : false;
 
   constructor(client: Stan) {
     this.client = client;
@@ -20,6 +20,7 @@ export abstract class Publisher<T extends Event> {
   publish(data: T['data']): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        console.log('logMsgs :', this.logMsgs);
         if (err) {
           return reject(err);
         }
