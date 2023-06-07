84613676dc0878fb357b4c35f4b6f479777735af
TomVerkon <tverkon@gmail.com>
2023-06-07 17:20:55 -0400

    Updates

--- common/src/utils/create-message.ts
+++ common/src/utils/create-message.ts
@@ -3,10 +3,14 @@ import request from 'supertest';
 export const createMsg = (
   title: string,
   expected: string | number,
-  response: request.Response
+  returned: string | number,
+  response?: request.Response
 ): string => {
-  const resTxt = JSON.parse(response.text);
-  return `${title}\nexpected: ${expected}, returned: ${response.status.toString()}\n${JSON.stringify(
+  let resTxt = '';
+  if (response && response.text) {
+    resTxt = JSON.parse(response.text);
+  }
+  return `${title}\nexpected: ${expected}, returned: ${returned.toString()}\n${JSON.stringify(
     resTxt,
     null,
     2
