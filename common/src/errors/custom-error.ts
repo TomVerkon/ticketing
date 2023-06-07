c4b15ff2a32d2cf00a4b9e59d8c37a6a64375da1
TomVerkon <tverkon@gmail.com>
2023-06-07 12:34:29 -0400

    Updates

--- common/src/errors/custom-error.ts
+++ common/src/errors/custom-error.ts
@@ -8,7 +8,7 @@ export interface ErrorMessage {
 export type ErrorMessages = ErrorMessage[];
 
 export abstract class CustomError extends Error {
-  abstract statusCode: ErrorStatusCode;
+  // abstract statusCode: ErrorStatusCode;
 
   constructor(message: string) {
     super(message);
