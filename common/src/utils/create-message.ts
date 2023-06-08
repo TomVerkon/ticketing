21d2e748d032ab5dbb2ab383ff04cae5073f0e1b
TomVerkon <tverkon@gmail.com>
2023-06-08 14:08:47 -0400

    Updates

--- common/src/utils/create-message.ts
+++ common/src/utils/create-message.ts
@@ -4,20 +4,19 @@ import request from 'supertest';
  * This function in meant to display formatted output while testing
  * @param {string} title - Test name can be obtained by calling expect.getState().currentTestName
  * @param {string | number} expected - The expected result
- * @param {string | number} returned - The returned result
+ * @param {string | number} actual - The actual result
  * @param {string} response? - pass response.text in to format/display what is returned to the client
  */
 export const createMsg = (
   title: string,
   expected: string | number,
-  returned: string | number,
+  actual: string | number,
   response?: string
 ): string => {
-  let resTxt = null;
+  const msgPrefix = `${title}\nexpected: ${expected.toString()}, returned: ${actual.toString()}`;
+  let msgSuffix = '';
   if (response) {
-    resTxt = JSON.stringify(JSON.parse(response), null, 2);
+    msgSuffix = `\n${JSON.stringify(JSON.parse(response), null, 2)}`;
   }
-  const msgPrefix = `${title}\nexpected: ${expected}, returned: ${returned.toString()}`;
-  const msgSufix = msgPrefix + resTxt ? resTxt : '';
-  return msgPrefix + msgSufix;
+  return `${msgPrefix}${msgSuffix}`;
 };
